745f81d483e09233888a729f8a1eca3c8e54a132
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-14 17:51:57 -0300

    #3 add api shops/name

--- src/routes/shops.routes.js
+++ src/routes/shops.routes.js
@@ -3,8 +3,8 @@ import Shops from './../models/shops.model';
 
 const router = Router();
 
-router.get('/search', async (req, res, _next) => {
-    const shop = await Shops.findOne({name: req.query.name});
+router.get('/:name', async (req, res, _next) => {
+    const shop = await Shops.findOne({name: req.params.name});
 
     if (shop) res.json(shop);
     else res.status(404).send('Shop not found');
