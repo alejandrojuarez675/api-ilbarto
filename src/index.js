81aaabc58a72b5e79dcd2bbe837ec07d16620022
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-14 17:30:01 -0300

    #3 add api shops/search?name

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import shopsRouter from './routes/shops.routes';
 const app = express();
 
 // middlewares
+app.use((_req, res, next) => {
+    res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'sid, TrackId, FlowId, corpid, Origin, X-Requested-With, Content-Type, Accept, Authorization, PaymentIntention');
+    next();
+});
 app.use(morgan('dev'));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
